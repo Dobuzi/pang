46263aa0d2b470a706f1bc99d86c38d916263c31
Dobuzi <kimjw7981@gmail.com>
2020-11-16 20:57:12 +0900

    Initial Commit

---
+++ pang/pangApp.swift
@@ -0,0 +1,17 @@
+//
+//  pangApp.swift
+//  pang
+//
+//  Created by 김종원 on 2020/11/16.
+//
+
+import SwiftUI
+
+@main
+struct pangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
